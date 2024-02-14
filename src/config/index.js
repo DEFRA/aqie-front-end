33cc37aabe490e09aa1d391c7015c0745a706266
ulysses <ulysses.alvarez@cognizant.com>
2024-02-14 16:50:29 +0000

    created manual prefixes for routers.

--- src/config/index.js
+++ src/config/index.js
@@ -41,7 +41,7 @@ const config = convict({
   appPathPrefix: {
     doc: 'Application url path prefix',
     format: String,
-    default: '',
+    default: '/aqie-front-end',
     env: 'APP_PATH_PREFIX'
   },
   isProduction: {
--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -1,9 +1,14 @@
+import { config } from '~/src/config'
+
+const prefix = config.get('appPathPrefix')
+
 const homeController = {
   handler: (request, h) => {
     return h.view('home/index', {
       pageTitle: 'Check local air quality',
       heading: 'Check local air quality',
-      page: 'homepage'
+      page: 'homepage',
+      prefix
     })
   }
 }
