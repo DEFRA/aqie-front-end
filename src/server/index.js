d1bb326cc51c616e6e9856fd77228811c3e3cbd2
ulysses <ulysses.alvarez@cognizant.com>
2024-02-14 15:18:30 +0000

    fixing prefix temporally and remove unit test for navigation temporally.

--- src/server/index.js
+++ src/server/index.js
@@ -44,9 +44,7 @@ async function createServer() {
     await server.register(secureContext)
   }
 
-  await server.register(router, {
-    routes: { prefix: config.get('appPathPrefix') }
-  })
+  await server.register(router, {})
 
   await server.register(nunjucksConfig)
 
