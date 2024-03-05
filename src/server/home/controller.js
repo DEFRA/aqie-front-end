9602bed5382883ddbe0be09f7dea350508b62530
ulysses <ulysses.alvarez@cognizant.com>
2024-03-05 16:44:57 +0000

    login error handling and fix library cookie bug.

--- src/server/home/controller.js
+++ src/server/home/controller.js
@@ -3,11 +3,17 @@ const homeController = {
     if (request.auth.isAuthenticated) {
       return h.redirect('/aqie-front-end/check-local-air-quality')
     } else {
+      const errors = request.yar.get('errors')
+      const errorMessage = request.yar.get('errorMessage')
+      request.yar.set('errors', null)
+      request.yar.set('errorMessage', null)
       return h.view('home/index', {
         pageTitle: 'Check local air quality',
         heading: 'Check local air quality',
         page: 'home',
-        serviceName: 'Check local air quality'
+        serviceName: 'Check local air quality',
+        errors: errors?.errors,
+        errorMessage: errorMessage?.errorMessage
       })
     }
   }
@@ -19,6 +25,22 @@ const loginController = {
       request.cookieAuth.set({ password: request.payload.password })
       return h.redirect('/aqie-front-end/check-local-air-quality')
     } else {
+      request.yar.set('errors', {
+        errors: {
+          titleText: 'There is a problem',
+          errorList: [
+            {
+              text: 'The password is not correct',
+              href: '#'
+            }
+          ]
+        }
+      })
+      request.yar.set('errorMessage', {
+        errorMessage: {
+          text: 'The password is not correct'
+        }
+      })
       return h.redirect('/aqie-front-end')
     }
   }
