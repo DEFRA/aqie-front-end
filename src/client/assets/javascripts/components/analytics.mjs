b913e1ad01461c86566f5175fd3c1d757ab47081
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2024-08-04 14:06:32 +0100

    sonat linter fixes.

--- src/client/assets/javascripts/components/analytics.mjs
+++ src/client/assets/javascripts/components/analytics.mjs
@@ -1,64 +1,66 @@
 // @ts-nocheck
 export default function loadAnalytics() {
-  if (!window.ga || !window.ga.loaded) {
-    window.dataLayer = window.dataLayer || []
-    const gtagCall = function gtag() {
-      window.dataLayer.push(arguments)
+  (function(){
+    if (!window.ga || !window.ga.loaded) {
+      window.dataLayer = window.dataLayer || []
+      function gtag() {
+        window.dataLayer.push(arguments)
+      }
+      
+      if(localStorage.getItem('consentMode') === null){
+        gtag('consent', 'default', {
+          'ad_storage': 'denied',
+          'analytics_storage': 'denied',
+          'personalization_storage': 'denied',
+          'functionality_storage': 'denied',
+          'security_storage': 'denied',
+        })
+      } else {
+        gtag('consent', 'default', JSON.parse(localStorage.getItem('consentMode')))
+      }
+      // prettier-ignore
+      ;(function (w, d, s, l, i) {
+        w[l] = w[l] || []
+        w[l].push({
+          'gtm.start': new Date().getTime(),
+          event: 'gtm.js'
+        })
+        ///
+        const noscript = document.createElement('noscript')
+        const iframe = document.createElement('iframe')
+        iframe.setAttribute("height", "0")
+        iframe.setAttribute("width", "0")
+        iframe.setAttribute("style", "display:none;visibility:hidden")
+        iframe.async = true
+        iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-PBFV8FNC"
+        noscript.appendChild(iframe)
+        document.body.appendChild(noscript)
+        ///
+        const k = d.createElement(s)
+        k.async = true
+        k.src = "https://www.googletagmanager.com/gtag/js?id=G-8CMZBTDQBC"
+        document.body.appendChild(k)
+        const urlParams = new URLSearchParams(window.location.search);
+        const userId = urlParams.get('userId');
+        const utm_source = urlParams.get('utm_source');
+        gtag('js', new Date())
+        gtag('config', 'G-8CMZBTDQBC',{
+          'event': 'userData',
+          'userId': userId,
+          'utm_source': utm_source
+        })
+        window.dataLayer.push({
+          'event': 'userData',
+          'userId': userId,
+          'utm_source': utm_source
+        })
+        ///
+        const f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+        f.parentNode.insertBefore(j, f)
+      })(window, document, 'script', 'dataLayer', 'GTM-PBFV8FNC')
     }
-    gtagCall()
-    if(localStorage.getItem('consentMode') === null){
-      gtag('consent', 'default', {
-        'ad_storage': 'denied',
-        'analytics_storage': 'denied',
-        'personalization_storage': 'denied',
-        'functionality_storage': 'denied',
-        'security_storage': 'denied',
-      })
-    } else {
-      gtag('consent', 'default', JSON.parse(localStorage.getItem('consentMode')))
-    }
-    // prettier-ignore
-    ;(function (w, d, s, l, i) {
-      w[l] = w[l] || []
-      w[l].push({
-        'gtm.start': new Date().getTime(),
-        event: 'gtm.js'
-      })
-      ///
-      const noscript = document.createElement('noscript')
-      const iframe = document.createElement('iframe')
-      iframe.setAttribute("height", "0")
-      iframe.setAttribute("width", "0")
-      iframe.setAttribute("style", "display:none;visibility:hidden")
-      iframe.async = true
-      iframe.src = "https://www.googletagmanager.com/ns.html?id=GTM-PBFV8FNC"
-      noscript.appendChild(iframe)
-      document.body.appendChild(noscript)
-      ///
-      const k = d.createElement(s)
-      k.async = true
-      k.src = "https://www.googletagmanager.com/gtag/js?id=G-8CMZBTDQBC"
-      document.body.appendChild(k)
-      const urlParams = new URLSearchParams(window.location.search);
-      const userId = urlParams.get('userId');
-      const utm_source = urlParams.get('utm_source');
-      gtag('js', new Date())
-      gtag('config', 'G-8CMZBTDQBC',{
-        'event': 'userData',
-        'userId': userId,
-        'utm_source': utm_source
-      })
-      window.dataLayer.push({
-        'event': 'userData',
-        'userId': userId,
-        'utm_source': utm_source
-      })
-      ///
-      const f = d.getElementsByTagName(s)[0],
-      j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''
-      j.async = true;
-      j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
-      f.parentNode.insertBefore(j, f)
-    })(window, document, 'script', 'dataLayer', 'GTM-PBFV8FNC')
-  }
+  })()
 }
