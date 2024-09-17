48f9ec83d3810211af1b3819e3fb62c05cd1abff
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2024-09-17 14:43:42 +0100

    remove more error cach handler.

--- src/server/locations/helpers/get-nearest-location.js
+++ src/server/locations/helpers/get-nearest-location.js
@@ -60,8 +60,8 @@ function getNearestLocation(
     `:::::::::::::::;;  nearestLocationsRangeCal 2 :::::::::::::::::: ${nearestLocationsRangeCal}`
   )
   // TODO select and filter locations and pollutants which are not null or don't have exceptions
-  const nearestLocationsRange = nearestLocationsRangeCal
-    .reduce((acc, curr, index) => {
+  const nearestLocationsRange = nearestLocationsRangeCal.reduce(
+    (acc, curr, index) => {
       const newpollutants = []
       const getDistance =
         geolib.getDistance(
@@ -71,96 +71,68 @@ function getNearestLocation(
             longitude: curr.location.coordinates[1]
           }
         ) * 0.000621371192
-      Object.keys(curr.pollutants)
-        .forEach((pollutant) => {
-          const polValue = curr.pollutants[pollutant].value
-          if (polValue !== null && polValue !== -99 && polValue !== '0') {
-            const { getDaqi, getBand } =
-              lang === 'cy'
-                ? getPollutantLevelCy(polValue, pollutant)
-                : getPollutantLevel(polValue, pollutant)
-            const formatDate = moment(
-              curr.pollutants[pollutant].time.date
-            ).format('ha')
-            Object.assign(newpollutants, {
-              [pollutant]: {
-                exception: curr.pollutants[pollutant].exception,
-                featureOfInterest: curr.pollutants[pollutant].featureOfInterest,
-                time: { date: formatDate },
-                value: polValue,
-                daqi: getDaqi,
-                band: getBand
-              }
-            }).catch((err) => {
-              logger.error(
-                `Failed to fetch nearest locations 1a: ${JSON.stringify(err)}`
-              )
-              throw new Error('Failed to fetch nearest locations 1a')
-            })
-          }
-        })
-        .catch((err) => {
-          logger.error(
-            `Failed to fetch nearest locations 1: ${JSON.stringify(err)}`
-          )
-          throw new Error('Failed to fetch nearest locations 1')
-        })
+      Object.keys(curr.pollutants).forEach((pollutant) => {
+        const polValue = curr.pollutants[pollutant].value
+        if (polValue !== null && polValue !== -99 && polValue !== '0') {
+          const { getDaqi, getBand } =
+            lang === 'cy'
+              ? getPollutantLevelCy(polValue, pollutant)
+              : getPollutantLevel(polValue, pollutant)
+          const formatDate = moment(
+            curr.pollutants[pollutant].time.date
+          ).format('ha')
+          Object.assign(newpollutants, {
+            [pollutant]: {
+              exception: curr.pollutants[pollutant].exception,
+              featureOfInterest: curr.pollutants[pollutant].featureOfInterest,
+              time: { date: formatDate },
+              value: polValue,
+              daqi: getDaqi,
+              band: getBand
+            }
+          })
+        }
+      })
       if (Object.keys(newpollutants).length !== 0) {
-        acc
-          .push({
-            area: curr.area,
-            areaType: curr.areaType,
-            location: {
-              type: curr.location.type,
-              coordinates: [
-                curr.location.coordinates[0],
-                curr.location.coordinates[1]
-              ]
-            },
-            id: curr.name.replaceAll(' ', ''),
-            name: curr.name,
-            updated: curr.updated,
-            distance: getDistance.toFixed(1),
-            pollutants: { ...newpollutants }
-          })
-          .catch((err) => {
-            logger.error(
-              `Failed to fetch nearest locations 2a: ${JSON.stringify(err)}`
-            )
-            throw new Error('Failed to fetch nearest locations 2a')
-          })
+        acc.push({
+          area: curr.area,
+          areaType: curr.areaType,
+          location: {
+            type: curr.location.type,
+            coordinates: [
+              curr.location.coordinates[0],
+              curr.location.coordinates[1]
+            ]
+          },
+          id: curr.name.replaceAll(' ', ''),
+          name: curr.name,
+          updated: curr.updated,
+          distance: getDistance.toFixed(1),
+          pollutants: { ...newpollutants }
+        })
       }
       acc.sort((a, b) => parseFloat(a.distance) - parseFloat(b.distance))
       return acc
-    }, [])
-    .catch((err) => {
-      logger.error(
-        `Failed to fetch nearest locations 2: ${JSON.stringify(err)}`
-      )
-      throw new Error('Failed to fetch nearest locations 2')
-    })
+    },
+    []
+  )
 
   const forecastDay = moment.tz('Europe/London').format('dddd').substring(0, 3)
   const forecastNum =
     matches.length !== 0
-      ? nearestLocation
-          .map((current) => {
-            let todayDate = []
-            const otherdays = []
+      ? nearestLocation.map((current) => {
+          let todayDate = []
+          const otherdays = []
 
-            current.forecast.forEach(({ day, value }) => {
-              if (day === forecastDay) {
-                todayDate = [{ today: value }]
-              } else {
-                otherdays.push({ [day]: value })
-              }
-            })
-            return [...todayDate, ...otherdays]
+          current.forecast.forEach(({ day, value }) => {
+            if (day === forecastDay) {
+              todayDate = [{ today: value }]
+            } else {
+              otherdays.push({ [day]: value })
+            }
           })
-          .catch((err) => {
-            logger.error(`Failed to fetch forecast: ${JSON.stringify(err)}`)
-            throw new Error('Failed to fetch forecast')
-          })
+          return [...todayDate, ...otherdays]
+        })
       : 0
   return { forecastNum, nearestLocationsRange }
 }
