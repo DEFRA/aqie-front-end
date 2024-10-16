b762e4f852bb07aedef156960a3e8d87547f26c7
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2024-10-16 10:48:38 +0100

    accessibility and uppercase for welsh.

--- src/server/locations/cy/controller.js
+++ src/server/locations/cy/controller.js
@@ -10,6 +10,7 @@ import { fetchData } from '~/src/server/locations/helpers/fetch-data'
 import { welsh, calendarWelsh } from '~/src/server/data/cy/cy.js'
 import { calendarEnglish } from '~/src/server/data/en/en.js'
 import moment from 'moment-timezone'
+import { sentenceCase } from '~/src/server/common/helpers/sentence-case'
 
 const logger = createLogger()
 const getLocationDataController = {
@@ -344,7 +345,7 @@ const getLocationDataController = {
         const locationData = {
           GAZETTEER_ENTRY: {
             NAME1: results[0].postcode,
-            DISTRICT_BOROUGH: results[0].administrativeArea,
+            DISTRICT_BOROUGH: sentenceCase(results[0].administrativeArea),
             LONGITUDE: results[0].xCoordinate,
             LATITUDE: results[0].yCoordinate
           }
