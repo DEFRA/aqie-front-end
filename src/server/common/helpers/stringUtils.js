eab3d5afaa832a37860f0547f15e46b3db669427
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2024-12-05 13:04:49 +0000

    prettier format.

--- src/server/common/helpers/stringUtils.js
+++ src/server/common/helpers/stringUtils.js
@@ -1,20 +1,20 @@
 // Define and export the firstLetterUppercase function
 export const firstLetterUppercase = (str) => {
-    // Split the string at the first comma
-    const [beforeComma, afterComma] = str.split(/,(.+)/)
-  
-    // Process the part after the comma
-    const processedAfterComma = afterComma
-      .toLowerCase()
-      .split(' ')
-      .map(word => {
-        if (word !== 'and' && word !== 'the' && word !== 'of') {
-          return word.charAt(0).toUpperCase() + word.slice(1)
-        }
-        return word
-      })
-      .join(' ')
-  
-    // Concatenate the unprocessed part with the processed part
-    return `${beforeComma},${processedAfterComma}`
-  }
+  // Split the string at the first comma
+  const [beforeComma, afterComma] = str.split(/,(.+)/)
+
+  // Process the part after the comma
+  const processedAfterComma = afterComma
+    .toLowerCase()
+    .split(' ')
+    .map((word) => {
+      if (word !== 'and' && word !== 'the' && word !== 'of') {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      }
+      return word
+    })
+    .join(' ')
+
+  // Concatenate the unprocessed part with the processed part
+  return `${beforeComma},${processedAfterComma}`
+}
