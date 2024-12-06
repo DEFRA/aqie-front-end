ef7a63530cc9d33fa8d9b6d5c50ff14676622670
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2024-12-06 11:07:04 +0000

    fixed ni title case for both header and browser page.

--- src/server/common/helpers/stringUtils.js
+++ src/server/common/helpers/stringUtils.js
@@ -1,11 +1,10 @@
 // Define and export the firstLetterUppercase function
 export const firstLetterUppercase = (str) => {
-  const isNumeric = (str) => !isNaN(str)
+  const isNumeric = (str) => /[0-9]/.test(str)
   const hasNumber = isNumeric(str)
   if (hasNumber) {
     // Split the string at the first comma
     const [beforeComma, afterComma] = str.split(/,(.+)/)
-
     // Process the part after the comma
     const processedAfterComma = afterComma
       .toLowerCase()
@@ -17,9 +16,17 @@ export const firstLetterUppercase = (str) => {
         return word
       })
       .join(' ')
-
     // Concatenate the unprocessed part with the processed part
     return `${beforeComma},${processedAfterComma}`
   }
-  return str.charAt(0).toUpperCase() + str.slice(1)
+  return str
+    .toLowerCase()
+    .split(' ')
+    .map((word) => {
+      if (word !== 'and' && word !== 'the' && word !== 'of') {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      }
+      return word
+    })
+    .join(' ')
 }
