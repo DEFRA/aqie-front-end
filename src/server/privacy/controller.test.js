dd760aa964d84118013e3a34494805f35b7c2e46
Saranya <Saranya.Vinayagam@cognizant.com>
2025-01-14 16:15:01 +0000

    committing unit test cases for search-location and privacy

---
+++ src/server/privacy/controller.test.js
@@ -0,0 +1,44 @@
+import { english } from '../data/en/en.js'
+import { privacyController } from './controller.js'
+
+describe('privacy controller - English', () => {
+  let mockRequest
+  let mockH
+  const mockContent = english
+  beforeEach(() => {
+    mockRequest = {
+      query: {}
+    }
+    mockH = {
+      redirect: jest.fn().mockReturnValue('redirected'),
+      view: jest.fn().mockReturnValue('view rendered')
+    }
+  })
+
+  it('should redirect to the Welsh version if the language is "cy"', () => {
+    mockRequest.query.lang = 'cy'
+    const result = privacyController.handler(mockRequest, mockH)
+    expect(result).toBe('redirected')
+    expect(mockH.redirect).toHaveBeenCalledWith('/preifatrwydd/cy?lang=cy')
+  })
+
+  it('should render the privacy page with the necessary data', () => {
+    mockRequest.query.lang = 'en'
+    const result = privacyController.handler(mockRequest, mockH)
+    expect(result).toBe('view rendered')
+    expect(mockH.view).toHaveBeenCalledWith('privacy/index', {
+      pageTitle: mockContent.footer.privacy.pageTitle,
+      title: mockContent.footer.privacy.title,
+      heading: mockContent.footer.privacy.heading,
+      headings: mockContent.footer.privacy.headings,
+      paragraphs: mockContent.footer.privacy.paragraphs,
+      displayBacklink: false,
+      phaseBanner: mockContent.phaseBanner,
+      footerTxt: mockContent.footerTxt,
+      cookieBanner: mockContent.cookieBanner,
+      serviceName: mockContent.multipleLocations.serviceName,
+      page: 'privacy',
+      lang: mockRequest.query.lang
+    })
+  })
+})
