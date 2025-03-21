9f4b8b1f60afcd0fcad8a3f14f68048ab224afba
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2025-03-21 10:12:54 +0000

    cleaning processMatch function.

---
+++ src/server/locations/helpers/search-terms-unitary.js
@@ -0,0 +1,49 @@
+function searchTermsAndUnitary(
+  userLocation,
+  name1,
+  name2,
+  secondSearchTerm,
+  unitary,
+  exactWordFirstTerm,
+  exactWordSecondTerm
+) {
+  const normalizeString = (str) => str?.toUpperCase().replace(/\s+/g, '')
+  if (name2) {
+    if (secondSearchTerm === 'UNDEFINED') {
+      return (
+        name2?.includes(normalizeString(userLocation)) ||
+        normalizeString(userLocation).includes(name2)
+      )
+    }
+    if (secondSearchTerm !== 'UNDEFINED') {
+      return (
+        name2?.includes(normalizeString(userLocation)) &&
+        normalizeString(userLocation).includes(name2) &&
+        secondSearchTerm.includes(unitary) &&
+        unitary?.includes(secondSearchTerm)
+      )
+    }
+  }
+  if (secondSearchTerm === 'UNDEFINED') {
+    return (
+      name1?.includes(normalizeString(userLocation)) ||
+      normalizeString(userLocation).includes(name1)
+    )
+  }
+  if (!exactWordFirstTerm) {
+    return false
+  }
+  if (!exactWordSecondTerm) {
+    return false
+  }
+  return (
+    (name1?.includes(normalizeString(userLocation)) ||
+      normalizeString(userLocation).includes(name1)) &&
+    (normalizeString(secondSearchTerm).includes(unitary) ||
+      unitary?.includes(normalizeString(secondSearchTerm))) &&
+    exactWordFirstTerm &&
+    exactWordSecondTerm
+  )
+}
+
+export { searchTermsAndUnitary }
