443fddea2ebc750bd3e7f9fe93fc389048991c8b
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2025-06-23 12:39:04 +0100

    added unit test and refactored for creation of helper functions 3.

--- src/server/location-id/helpers/redirectToWelshLocation.js
+++ src/server/location-id/helpers/redirectToWelshLocation.js
@@ -1,7 +1,7 @@
 import { LANG_CY } from '~/src/server/data/constants'
 
 const redirectToWelshLocation = (query, locationId, h) => {
-  if (query?.lang === LANG_CY && !query?.searchTerms) {
+  if (query?.lang === LANG_CY && !query?.searchTerms && locationId) {
     return h.redirect(`/lleoliad/${locationId}/?lang=cy`)
   }
   return null
