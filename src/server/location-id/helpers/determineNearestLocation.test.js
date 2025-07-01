fb96771026a7ef3b91aee2496d55b8bc29d77bdb
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2025-07-01 22:21:18 +0100

    extra sonar quality code check.

--- src/server/location-id/helpers/determineNearestLocation.test.js
+++ src/server/location-id/helpers/determineNearestLocation.test.js
@@ -2,13 +2,17 @@ import { describe, it, expect } from 'vitest'
 
 describe('Determine Nearest Location Tests', () => {
   it('should determine the nearest location correctly', () => {
-    const determineNearestLocation = (locations, currentLocation) => {
-      return locations.find(
-        (location) =>
-          location.distance ===
-          Math.min(...locations.map((loc) => loc.distance))
-      )
+    // '' Helper function to get the minimum distance from the locations array
+    const getMinimumDistance = (locations) => {
+      return Math.min(...locations.map((loc) => loc.distance))
     }
+
+    // '' Determines the nearest location based on the minimum distance
+    const determineNearestLocation = (locations, currentLocation) => {
+      const minDistance = getMinimumDistance(locations)
+      return locations.find((location) => location.distance === minDistance)
+    }
+
     const locations = [
       { name: 'Cardiff', distance: 10 },
       { name: 'Swansea', distance: 5 },
