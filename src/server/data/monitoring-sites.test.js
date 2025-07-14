4ce9339fe9bb85b1fdd77e80cb5ccdbd8220bd47
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2025-07-14 12:38:34 +0100

    updated sonar cube config 2.

--- src/server/data/monitoring-sites.test.js
+++ src/server/data/monitoring-sites.test.js
@@ -1,5 +1,5 @@
 import { vi } from 'vitest'
-import { pollutantTypes, siteTypeDescriptions } from './monitoring-sites.js'
+import * as mock from './monitoring-sites.js'
 
 // Mock the monitoring-sites.js module using the imported constant
 vi.mock('./monitoring-sites.js', () => ({
@@ -49,47 +49,42 @@ vi.mock('./monitoring-sites.js', () => ({
 
 describe('pollutantTypes', () => {
   it('should have correct data for NO2', () => {
-    expect(pollutantTypes.NO2).toEqual({
+    expect(mock.pollutantTypes.NO2).toEqual({
       title: 'Nitrogen dioxide',
       href: '/pollutants/nitrogen-dioxide',
       low_range: '0 to 200'
     })
   })
-
   it('should have correct data for MP10', () => {
-    expect(pollutantTypes.MP10).toEqual({
+    expect(mock.pollutantTypes.MP10).toEqual({
       title: 'PM10',
       href: '/pollutants/particulate-matter-10',
       low_range: '0 to 50'
     })
   })
-
   it('should have correct data for GE10', () => {
-    expect(pollutantTypes.GE10).toEqual({
+    expect(mock.pollutantTypes.GE10).toEqual({
       title: 'PM10',
       href: '/pollutants/particulate-matter-10',
       low_range: '0 to 50'
     })
   })
-
   it('should have correct data for PM25', () => {
-    expect(pollutantTypes.PM25).toEqual({
+    expect(mock.pollutantTypes.PM25).toEqual({
       title: 'PM2.5',
       href: '/pollutants/particulate-matter-25',
       low_range: '0 to 35'
     })
   })
-
   it('should have correct data for O3', () => {
-    expect(pollutantTypes.O3).toEqual({
+    expect(mock.pollutantTypes.O3).toEqual({
       title: 'Ozone',
       href: '/pollutants/ozone',
       low_range: '0 to 100'
     })
   })
-
   it('should have correct data for SO2', () => {
-    expect(pollutantTypes.SO2).toEqual({
+    expect(mock.pollutantTypes.SO2).toEqual({
       title: 'Sulphur dioxide',
       href: '/pollutants/sulphur-dioxide',
       low_range: '0 to 100'
@@ -99,37 +94,32 @@ describe('pollutantTypes', () => {
 
 describe('siteTypeDescriptions', () => {
   it('should have correct description for Background Urban', () => {
-    expect(siteTypeDescriptions['Background Urban']).toBe(
+    expect(mock.siteTypeDescriptions['Background Urban']).toBe(
       'This monitoring area is located in a city or a town. It is located so pollutant measurements do not come from one specific source.'
     )
   })
-
   it('should have correct description for Background Suburban', () => {
-    expect(siteTypeDescriptions['Background Suburban']).toBe(
+    expect(mock.siteTypeDescriptions['Background Suburban']).toBe(
       'This monitoring area is on the outskirts of an urban area or in an area of its own. It is located so pollutant measurements do not come from one specific source.'
     )
   })
-
   it('should have correct description for Background Rural', () => {
-    expect(siteTypeDescriptions['Background Rural']).toBe(
+    expect(mock.siteTypeDescriptions['Background Rural']).toBe(
       'This monitoring area is in small settlements or areas with natural ecosystems, forests or crops. It is located so pollutant measurements do not come from one specific source.'
     )
   })
-
   it('should have correct description for Industrial Suburban', () => {
-    expect(siteTypeDescriptions['Industrial Suburban']).toBe(
+    expect(mock.siteTypeDescriptions['Industrial Suburban']).toBe(
       'This monitoring area is on the outskirts of an urban area or in an area of its own and downwind of an industrial source.'
     )
   })
-
   it('should have correct description for Industrial Urban', () => {
-    expect(siteTypeDescriptions['Industrial Urban']).toBe(
+    expect(mock.siteTypeDescriptions['Industrial Urban']).toBe(
       'This monitoring area is located in a city or a town downwind of an industrial source.'
     )
   })
-
   it('should have correct description for Traffic Urban', () => {
-    expect(siteTypeDescriptions['Traffic Urban']).toBe(
+    expect(mock.siteTypeDescriptions['Traffic Urban']).toBe(
       'This monitoring area is located in a city or a town close to roads, motorways or highways.'
     )
   })
