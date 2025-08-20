dba9205c72c1bb395a0f2c93c2670e076bd5f67a
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2025-08-20 22:57:47 +0100

    more logs to check 7

--- src/server/locations/helpers/middleware-helpers.js
+++ src/server/locations/helpers/middleware-helpers.js
@@ -38,6 +38,7 @@ const handleSingleMatch = (
   }
 ) => {
   const customId = selectedMatches.length === 1 ? urlRoute : headerTitleRoute // Use the helper function to generate the custom ID
+  logger.info(`getForecasts?.forecastsxxxx2: ${JSON.stringify(getForecasts?.forecasts)}`)
   request.yar.set('locationData', {
     results: selectedMatches,
     getForecasts: getForecasts?.forecasts,
@@ -88,6 +89,7 @@ const handleMultipleMatches = (
 ) => {
   const resolvedLocationNameOrPostcode =
     locationNameOrPostcode ?? 'Unknown Location'
+  logger.info(`getForecasts?.forecastsxxxx: ${JSON.stringify(getForecasts?.forecasts)}`)
   request.yar.set('locationData', {
     results: selectedMatches,
     getForecasts: getForecasts?.forecasts,
