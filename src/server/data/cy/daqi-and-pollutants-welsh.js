9924ef938ab85b51e09b717ed90037e93abcb277
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2025-11-24 12:44:13 +0000

    Update health advice and exposure reduction links to use actual application routes
    - Add HEALTH_EFFECTS_ROUTE_EN and HEALTH_EFFECTS_ROUTE_CY constants to constants.js
    - Update exposureHtml in EN/CY daqi-and-pollutants files with new content structure
    - Move long-term exposure paragraph to top, add new 'You should try to reduce' paragraph
    - Replace prototype paths with route constants using {locationId} placeholder
    - Update high and veryHigh health advice in air-quality.js to use ACTIONS_REDUCE_EXPOSURE_ROUTE_EN
    - All links now use proper dynamic routes: /location/{locationId}/health-effects and /location/{locationId}/actions-reduce-exposure

--- src/server/data/cy/daqi-and-pollutants-welsh.js
+++ src/server/data/cy/daqi-and-pollutants-welsh.js
@@ -1,5 +1,9 @@
 ''
-import { WELSH_TITLE, ACTIONS_REDUCE_EXPOSURE_ROUTE_CY } from '../constants.js'
+import {
+  WELSH_TITLE,
+  ACTIONS_REDUCE_EXPOSURE_ROUTE_CY,
+  HEALTH_EFFECTS_ROUTE_CY
+} from '../constants.js'
 
 /**
  * Welsh translations for DAQI display and pollutant information pages
@@ -79,14 +83,14 @@ export const daqiTranslationsWelsh = {
     },
     // '' AQC-657: Adran cyngor ar leihau amlygiad (HTML trwy'r partial) - TODO: adolygu cyfieithiad
     exposureHtml: `<h2 class="govuk-heading-m">Sut y gallwch leihau eich amlygiad i lygredd aer</h2>
-<p>Ystyriwch y camau canlynol:</p>
+<p>Gall amlygiad hirdymor i lygredd aer (dros flynyddoedd) arwain at lawer o wahanol <a class="govuk-link" href="${HEALTH_EFFECTS_ROUTE_CY}?lang=cy">gyflyrau iechyd</a> ac ostwng disgwyliad oes.</p>
+<p>Dylech geisio lleihau eich amlygiad i lygredd aer i ddiogelu eich iechyd. Ystyriwch y camau canlynol:</p>
 <ul class="govuk-list govuk-list--bullet">
   <li>dewiswch lwybrau gyda llai o draffig, yn enwedig ar adegau prysur o'r diwrnod</li>
   <li>teithiwch i'r gwaith, cerddwch neu ymarferwch mewn parciau neu leoedd gwyrdd eraill, os nad ydych yn cael eich effeithio gan baill</li>
   <li>os ydych yn ymarfer dan do, gwnewch yn siŵr bod yr ystafell wedi'i hawyru'n dda</li>
 </ul>
-<p>Cael mwy o gyngor ar <a class="govuk-link" href="${ACTIONS_REDUCE_EXPOSURE_ROUTE_CY}?lang=cy">gamau y gallwch eu cymryd i leihau eich amlygiad i lygredd aer</a>.</p>
-<p>Gall amlygiad hirdymor i lygredd aer (dros flynyddoedd) arwain at lawer o wahanol <a class="govuk-link" href="/proto-dev-new/health-effects?locationName=&parentArea=">gyflyrau iechyd</a> ac ostwng disgwyliad oes.</p>`
+<p>Cael mwy o gyngor ar <a class="govuk-link" href="${ACTIONS_REDUCE_EXPOSURE_ROUTE_CY}?lang=cy">gamau y gallwch eu cymryd i leihau eich amlygiad i lygredd aer</a>.</p>`
   }
 }
 
