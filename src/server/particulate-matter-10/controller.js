582dd661fc9ab9de538f697b3c0dd57197a658ee
Ulysses Alvarez <ulysses.alvarez@cognizant.com>
2026-03-18 21:02:05 +0000

    fix(server): restore pollutant controller shared handler

--- src/server/particulate-matter-10/controller.js
+++ src/server/particulate-matter-10/controller.js
@@ -1,36 +1,8 @@
-import { english } from '../data/en/en.js'
-import { LANG_CY, LANG_EN, REDIRECT_STATUS_CODE } from '../data/constants.js'
-import { getAirQualitySiteUrl } from '../common/helpers/get-site-url.js'
+import { createPollutantHandler } from '../common/helpers/pollutant-controller-helper.js'
 
 const particulateMatter10Controller = {
-  handler: (request, h) => {
-    const { particulateMatter10 } = english.pollutants
-    const { footerTxt, cookieBanner, phaseBanner, multipleLocations } = english
-    const { query } = request
-    const lang = LANG_EN
-    const metaSiteUrl = getAirQualitySiteUrl(request)
-
-    if (query?.lang && query?.lang === LANG_CY) {
-      return h
-        .redirect(`/llygryddion/mater-gronynnol-10/cy?lang=cy`)
-        .code(REDIRECT_STATUS_CODE)
-    }
-
-    return h.view('particulate-matter-10/index', {
-      pageTitle: particulateMatter10.pageTitle,
-      description: particulateMatter10.description,
-      metaSiteUrl,
-      particulateMatter10,
-      page: 'particulate matter 10',
-      displayBacklink: false,
-      phaseBanner,
-      footerTxt,
-      cookieBanner,
-      serviceName: multipleLocations.serviceName,
-      currentPath: '/pollutants/particulate-matter-10',
-      lang: query.lang ?? lang
-    })
-  }
+  handler: (request, h) =>
+    createPollutantHandler('particulateMatter10', request, h)
 }
 
 export { particulateMatter10Controller }
