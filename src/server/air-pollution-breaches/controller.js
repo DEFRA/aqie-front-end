ed163a6b5bc0219f50a8503066103b0280830cb8
MoCognizant <mohamedabdullahi.mohamed@cognizant.com>
2026-05-19 20:11:20 +0100

    Sonar Fix

--- src/server/air-pollution-breaches/controller.js
+++ src/server/air-pollution-breaches/controller.js
@@ -120,6 +120,34 @@ function buildBackLinkData({
   }
 }
 
+function buildLocationLinks(
+  locationId,
+  locationName,
+  searchTerms,
+  pastBreaches
+) {
+  const actionsLink = locationId
+    ? `/location/${locationId}/actions-reduce-exposure?lang=en`
+    : '#'
+
+  const locationQuerySuffix = [
+    locationId ? `locationId=${encodeURIComponent(locationId)}` : '',
+    locationName ? `locationName=${encodeURIComponent(locationName)}` : '',
+    searchTerms ? `searchTerms=${encodeURIComponent(searchTerms)}` : ''
+  ]
+    .filter(Boolean)
+    .join('&')
+
+  const processedPastBreaches = pastBreaches.map((breach) => ({
+    ...breach,
+    pollutantLink: locationQuerySuffix
+      ? `${breach.pollutantLink}&${locationQuerySuffix}`
+      : breach.pollutantLink
+  }))
+
+  return { actionsLink, processedPastBreaches }
+}
+
 function getViewModel(
   content,
   activeBreaches,
@@ -140,24 +168,12 @@ function getViewModel(
     backlink
   })
 
-  const actionsLink = locationId
-    ? `/location/${locationId}/actions-reduce-exposure?lang=en`
-    : '#'
-
-  const locationQuerySuffix = [
-    locationId ? `locationId=${encodeURIComponent(locationId)}` : '',
-    locationName ? `locationName=${encodeURIComponent(locationName)}` : '',
-    searchTerms ? `searchTerms=${encodeURIComponent(searchTerms)}` : ''
-  ]
-    .filter(Boolean)
-    .join('&')
-
-  const processedPastBreaches = pastBreaches.map((breach) => ({
-    ...breach,
-    pollutantLink: locationQuerySuffix
-      ? `${breach.pollutantLink}&${locationQuerySuffix}`
-      : breach.pollutantLink
-  }))
+  const { actionsLink, processedPastBreaches } = buildLocationLinks(
+    locationId,
+    locationName,
+    searchTerms,
+    pastBreaches
+  )
 
   return {
     pageTitle: content.pageTitle,
