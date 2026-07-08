3b73fed612113583776e99aad40dce8b6ea0b158
Stewart Jumbe <stewarjumbe@gmail.com>
2026-07-08 12:46:51 +0100

    move google analytics implentation from page.njk to analytics to align with previous GA implementation which is only applied after the user consents. Two GTM tags are implemented for the time being, with the intention of removing the old one GTM-PBFV8FNC in the future

--- src/client/assets/javascripts/components/analytics.mjs
+++ src/client/assets/javascripts/components/analytics.mjs
@@ -63,5 +63,23 @@ export default function loadAnalytics() {
         j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
         f.parentNode.insertBefore(j, f)
       })(window, document, 'script', 'dataLayer', 'GTM-PBFV8FNC')
+    ///
+    // prettier-ignore
+    ;(function (w, d, s, l, i) {
+        const noscript2 = document.createElement('noscript')
+        const iframe2 = document.createElement('iframe')
+        iframe2.setAttribute('height', '0')
+        iframe2.setAttribute('width', '0')
+        iframe2.setAttribute('style', 'display:none;visibility:hidden')
+        iframe2.src = 'https://www.googletagmanager.com/ns.html?id=' + i
+        noscript2.appendChild(iframe2)
+        document.body.appendChild(noscript2)
+        ///
+        const f = d.getElementsByTagName(s)[0],
+        j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : ''
+        j.async = true
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl
+        f.parentNode.insertBefore(j, f)
+      })(window, document, 'script', 'dataLayer', 'GTM-PCJNTC4H')
   }
 }
